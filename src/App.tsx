c64a6874cdb4d807259d658556648580642dfad7
Tetiana Diachenko <ct.tetianadiachenko@waterford.org>
2025-04-09 10:21:27 +0300

    delete unnecessary info in project

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import './scss/main.scss';
 const App = () => {
   return (
     <div className="container">
-      <div className="item">Item 1</div>
-      <div className="item">Item 2</div>
+
     </div>
   );
 };
