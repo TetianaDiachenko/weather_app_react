9783a874a16ed18f500ca64ccad5db063f83441f
Tetiana Diachenko <ct.tetianadiachenko@waterford.org>
2025-04-09 10:16:36 +0300

    fixed autoprefixer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './scss/main.scss';
 
 const App = () => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: ['> 1%', 'last 2 versions', 'not dead', 'ie 11', 'not op_mini all'],
-        }),
-      ],
-    },
-  },
 })
