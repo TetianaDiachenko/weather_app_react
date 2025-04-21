543c22dc3c2835341fe0a2b8a71a5fb1929497a9
Tetiana Diachenko <ct.tetianadiachenko@waterford.org>
2025-04-21 14:43:59 +0300

    added Header component

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,2 +1,24 @@
+// import React from 'react';
+import styles from './Header.module.scss';
+// import Search from '../../search/Search';
+// import SignInButton from '../../ui/SignInButton/SignInButton';
+// import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <div className={styles.left}>
+                    {/* <Search /> */}
+                </div>
+                <div className={styles.right}>
+                    {/* <ThemesSwitcher /> 
+                    <SignInButton /> */}
+                </div>                
+            </div>
+        </header>
+    )
+}   
+
+export default Header;
 
