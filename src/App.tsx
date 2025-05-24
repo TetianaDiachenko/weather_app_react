a85ac21cc10cb7c13921f363e1e4b2f91a47292e
Tetiana Diachenko <ct.tetianadiachenko@waterford.org>
2025-05-24 14:57:20 +0300

    made routing for main page with map by default

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/contexts/ThemeProvider";
 import Layout from "./components/layout/Layout";
 import { createBrowserRouter } from "react-router";
+import Main from "./pages/Main";
 import './App.scss'; 
 
 const router = createBrowserRouter([
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     element: <Layout />, 
     // errorElement: < NotFound />, // add
     children: [
+      { index: true, element: <Main /> } 
       // { path: "main", element: <Main />} // add
     ] },
 ],
