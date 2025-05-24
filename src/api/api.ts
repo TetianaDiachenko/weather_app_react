bdc90849b20611698855d25a91036e00258c88cf
Tetiana Diachenko <ct.tetianadiachenko@waterford.org>
2025-05-24 09:34:53 +0300

    added weather api

---
+++ src/api/api.ts
@@ -0,0 +1,26 @@
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+export type WeatherData = {
+    name: string;
+    coord: { lat: number; lon: number };
+    weather: { description: string; icon: string }[];
+    main: { temp: number };
+}
+
+async function fetchWeatherByCoords (
+    lat: number,
+    lon: number,
+    lang = 'en'
+): Promise<WeatherData> {
+    const url = `${BASE_URL}/weather?lat=${lat}&${lon}&units=metric&appid=${API_KEY}&lang${lang}`;
+
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`OpenWeather API error: ${res.statusText}`);
+    }
+    const data: WeatherData = await res.json();
+    return data;
+}
+
+export default { fetchWeatherByCoords };
